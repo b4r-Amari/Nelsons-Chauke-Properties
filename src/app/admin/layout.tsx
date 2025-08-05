ed3a9dce9af38a32f39a40025be42f16a66b582d
Tommy Chauke <topsecretestuff@gmail.com>
2025-08-05 21:46:51 +0000

    remove the header for the login please

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,8 @@
 
 import type { Metadata } from 'next';
+import { AuthProvider } from '@/context/auth-context';
+import { Toaster } from '@/components/ui/toaster';
+import '../globals.css';
 
 export const metadata: Metadata = {
   title: 'Admin - NC Properties',
@@ -12,8 +15,18 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      {children}
-    </>
+    <html lang="en">
+       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-body antialiased text-foreground/90">
+        <AuthProvider>
+            {children}
+            <Toaster />
+        </AuthProvider>
+      </body>
+    </html>
   );
 }
