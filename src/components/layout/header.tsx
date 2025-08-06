715bd2325a68f8110ea9a325cd84e458324d0492
Tommy Chauke <topsecretestuff@gmail.com>
2025-08-06 09:48:17 +0000

    the website will still shift to the left when i open the side bar

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -223,7 +223,7 @@ export function Header() {
                                       className={cn(
                                       "block rounded-md px-3 py-2 text-lg font-headline transition-colors w-full text-left",
                                       (pathname === link.href || (link.href !== "/" && pathname.startsWith(link.href)))
-                                          ? "bg-brand-bright text-white"
+                                          ? "bg-muted text-foreground"
                                           : "hover:bg-muted"
                                       )}
                                   >
