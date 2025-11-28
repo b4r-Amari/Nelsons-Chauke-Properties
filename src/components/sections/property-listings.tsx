64847440145b6d3d8ede76137a87cb9a8b03d413
Tommy Chauke <topsecretestuff@gmail.com>
2025-11-28 00:54:47 +0000

    use the colours of the website

--- src/components/sections/property-listings.tsx
+++ src/components/sections/property-listings.tsx
@@ -116,7 +116,7 @@ function PropertyListingsComponent({ status, pageDetails }: PropertyListingsProp
         </div>
       </section>
 
-      <div className="bg-brand-bright relative z-20 py-6">
+      <div className="bg-brand-deep relative z-20 py-6">
         <div className="container">
            <PropertyFilter 
             properties={allProperties} 
